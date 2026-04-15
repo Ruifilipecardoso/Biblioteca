e161e0352630aa74473279787e39a1d1f885dea5
mariaims11 <marimims2002@gmail.com>
2026-04-15 17:51:33 +0100

    Commit Maria

--- src/main/java/com/iefp/biblioteca/controller/BibliotecarioController.java
+++ src/main/java/com/iefp/biblioteca/controller/BibliotecarioController.java
@@ -1,4 +1,9 @@
 package com.iefp.biblioteca.controller;
 
 public class BibliotecarioController {
+    public class Bibliotecario {
+        private String nome;
+        private int id_bibliotecario;
+        private String password;
+    }
 }
--- src/main/java/com/iefp/biblioteca/model/Bibliotecario.java
+++ src/main/java/com/iefp/biblioteca/model/Bibliotecario.java
@@ -1,4 +1,28 @@
 package com.iefp.biblioteca.model;
 
 public class Bibliotecario {
+    private String nome;
+    private int id_bibliotecario;
+    private String password;
+
+//Construtor
+public Bibliotecario (String nome, int id_bibliotecario,String password){
+    this.nome = nome;
+    this.id_bibliotecario = id_bibliotecario;
+    this.password = password;
 }
+
+public String getNome(){
+    return nome;
+}
+
+public int getId_bibliotecario(){
+    return id_bibliotecario;
+}
+
+public String getPassword(){
+    return password;
+}
+}
+
+
