c88402f0e88f989988d80be35d139018e86112d0
mariaims11 <marimims2002@gmail.com>
2026-04-21 16:58:40 +0100

    Commit Maria

--- src/main/java/com/iefp/biblioteca/controller/BibliotecarioController.java
+++ src/main/java/com/iefp/biblioteca/controller/BibliotecarioController.java
@@ -1,9 +1,30 @@
 package com.iefp.biblioteca.controller;
 
+import com.iefp.biblioteca.model.Bibliotecario;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class BibliotecarioController {
-    public class Bibliotecario {
-        private String nome;
-        private int id_bibliotecario;
-        private String password;
+    private List<Bibliotecario> bibliotecario = new ArrayList<>();
+
+    @GetMapping("/login")
+    public String mostrarLogin(Model model){
+        model.addAttribute("bibliotecarios", bibliotecario);
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String fazerLogin(@RequestParam String nome, @RequestParam int id_bibliotecario,
+                                    @RequestParam String password){
+        bibliotecario.add(new Bibliotecario(nome, id_bibliotecario, password));
+        return "redirect:/login";
     }
 }
