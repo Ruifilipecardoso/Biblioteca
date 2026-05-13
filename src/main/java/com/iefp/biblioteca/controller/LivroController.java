1f80c4bc2129efad53934f6298e1c7d8baa6dcdf
Rui Filipe Cardoso <150164913+Ruifilipecardoso@users.noreply.github.com>
2026-05-13 15:27:21 +0100

    Livro completo

--- src/main/java/com/iefp/biblioteca/controller/LivroController.java
+++ src/main/java/com/iefp/biblioteca/controller/LivroController.java
@@ -1,24 +1,36 @@
 package com.iefp.biblioteca.controller;
 
 import com.iefp.biblioteca.model.Livro;
+import com.iefp.biblioteca.service.EmprestimoService;
 import com.iefp.biblioteca.service.LivroService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LivroController {
     private final LivroService livroService;
+    private final EmprestimoService emprestimoService;
 
-    public LivroController(LivroService livroService, LivroService livroService1) {
+    public LivroController(LivroService livroService, LivroService livroService1, EmprestimoService emprestimoService) {
         this.livroService = livroService;
+        this.emprestimoService = emprestimoService;
     }
 
     @GetMapping("/livros")
-    public String listarLivros(@RequestParam Long id, @RequestParam String titulo, @RequestParam String categoria, @RequestParam String autor, @RequestParam Integer isbn, @RequestParam String estado, @RequestParam Integer estoque) {
-        Livro livro = new Livro(null, titulo, categoria,autor, isbn, estado, estoque, null);
-        livroService.guardar(livro);
+    public String listarLivros(Model model) {
+        model.addAttribute("lista", livroService.listarLivros());
+        model.addAttribute("emprestimos", emprestimoService.listarEmprestimos());
+        return "livros";
+    }
+
+    @PostMapping("/livros")
+    public String adicionarLivro(String titulo, String categoria, String autor, Integer isbn, String estado, Integer estoque, Long emprestimoId) {
+        livroService.guardarLivro(titulo, categoria, autor, isbn, estado, estoque, emprestimoId);
         return "redirect:/livros";
     }
+
 }
--- src/main/java/com/iefp/biblioteca/service/LivroService.java
+++ src/main/java/com/iefp/biblioteca/service/LivroService.java
@@ -1,27 +1,40 @@
 package com.iefp.biblioteca.service;
 
+import com.iefp.biblioteca.model.Emprestimo;
 import com.iefp.biblioteca.model.Livro;
+import com.iefp.biblioteca.repository.EmprestimoRepository;
 import com.iefp.biblioteca.repository.LivroRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Service
 public class LivroService {
     private final LivroRepository livroRepository;
+    private final EmprestimoRepository emprestimoRepository;
 
-    public LivroService(LivroRepository livroRepository) {
+    public LivroService(LivroRepository livroRepository, EmprestimoRepository emprestimoRepository) {
         this.livroRepository = livroRepository;
+        this.emprestimoRepository = emprestimoRepository;
     }
 
-    public List<Livro> obterTodosLivros(){
-        return  livroRepository.findAll();
-    }
-
-    public void guardarLivro(Livro livro){
+    public void guardarLivro(String titulo, String categoria, String autor, Integer isbn, String estado, Integer estoque, Long emprestimoId){
+        Emprestimo emprestimo = emprestimoRepository.findById(emprestimoId).orElse(null);
+        Livro livro = new Livro();
+        livro.setTitulo(titulo);
+        livro.setCategoria(categoria);
+        livro.setAutor(autor);
+        livro.setIsbn(isbn);
+        livro.setEstado(estado);
+        livro.setEstoque(estoque);
         livroRepository.save(livro);
     }
 
-    public void guardar(Livro livro) {
+
+    public List<Livro> listarLivros(){
+        return  livroRepository.findAll();
     }
+
+
 }
